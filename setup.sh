bbf501d0f8117de834a4797e5948fbfa97650fa3
pontus <pontus@talkamatic.se>
2015-03-05 10:01:58 +0100

    replace bufexpl with minibufexpl

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ rm -fr vim-pathogen
 mkdir bundle
 cd bundle
 git clone https://github.com/mileszs/ack.vim.git
-git clone https://github.com/jlanzarotta/bufexplorer.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/sjl/gundo.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
@@ -16,3 +15,4 @@ git clone https://github.com/bling/vim-airline.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/bronson/vim-trailing-whitespace.git
 git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/fholgado/minibufexpl.vim.git
