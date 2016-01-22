cc63f8437dd4393e3b945be917df734384a72d49
pontus <pontus@talkamatic.se>
2016-01-22 09:38:50 +0100

    added gf syntax

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-ln -s vimrc ../.vimrc
+l -s vimrc ../.vimrc
 
 git clone https://github.com/tpope/vim-pathogen.git
 mv vim-pathogen/autoload .
@@ -16,3 +16,4 @@ git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/bronson/vim-trailing-whitespace.git
 git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/fholgado/minibufexpl.vim.git
+git clone https://github.com/gdetrez/vim-gf.git
