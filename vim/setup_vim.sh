d0bee31b8a1f02172a436555afa7c871297aca3e
pontus <you@example.com>
2019-06-30 21:56:38 +0200

    add easymotion, nrrwrgn, surround, zoomwin, gitgutter. Remapped leader. Started work on readme

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,8 +1,7 @@
-ln -s "${PWD}/vim/vimrc" ~/.vimrc
-
-
 mkdir -p ~/.vim
 
+ln -s "${PWD}/vim/vimrc" ~/.vimrc
+ln -s "${PWD}/vim/useful_bindings.txt" ~/.vim/useful_bindings.txt
 cp -r "${PWD}/vim/colors" ~/.vim/
 cd ~/.vim
 
@@ -15,16 +14,20 @@ cd ~/.vim/bundle
 git clone https://github.com/mileszs/ack.vim.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/jaxbot/semantic-highlight.vim.git
 git clone https://github.com/bling/vim-airline.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/bronson/vim-trailing-whitespace.git
-git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/fholgado/minibufexpl.vim.git
-git clone https://github.com/gdetrez/vim-gf.git
 git clone https://github.com/mbbill/undotree.git
 git clone https://github.com/szw/vim-tags.git
 git clone https://github.com/jceb/vim-orgmode.git
 git clone git://github.com/tpope/vim-speeddating.git #Dep for orgmode
 git clone git://github.com/majutsushi/tagbar
 git clone https://github.com/ludovicchabant/vim-gutentags
+git clone https://github.com/easymotion/vim-easymotion
+git clone https://github.com/chrisbra/NrrwRgn
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/airblade/vim-gitgutter
+git clone https://github.com/vim-scripts/ZoomWin
