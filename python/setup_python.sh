09d2353329b458058e9f89e087e2e1a57d5b0ad5
Pontus <pontus.lindstrom@seal-software.com>
2019-11-11 09:21:08 +0100

    add pyenv

--- python/setup_python.sh
+++ python/setup_python.sh
@@ -1,3 +1,4 @@
 python3 -m pip install --user pudb
 
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
