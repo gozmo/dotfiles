70439efe6178fa22a2e34b8fdaa8a890f1bff409
goz <goz@host.com>
2023-04-17 08:34:54 +0200

    remove python

--- python/setup_python.sh
+++
@@ -1,9 +0,0 @@
-python3 -m pip install --user pudb
-
-
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-
-mkdir -p "{HOME}"/.config/pudb
-ln -s "{HOME}"/.config/pudb/pudb.cfg "{PWD}"/python/pudb.cfg
