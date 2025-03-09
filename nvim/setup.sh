01fd6479515475d995e5807cd8e5a03706c34790
pontus <you@example.com>
2025-03-09 21:08:15 +0100

    update nvim setup, run latest bin

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,3 +1,11 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+mkdir -p ~/opt/nvim/
+mv nvim-linux-x86_64.appimage ~/opt/nvim/nvim
+
+
+
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
